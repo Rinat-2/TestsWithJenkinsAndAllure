590cc091fda9fd0cb61df175988f9e232774a917
Rinat <rinat.astr2@gmail.com>
2020-08-30 13:46:41 +0600

    added TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 public class TestBase {
     @BeforeAll
     @Step("Tests setup")
-    public void beforeAll() {
+    public static void beforeAll() {
         addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         Configuration.headless = true;
     }
