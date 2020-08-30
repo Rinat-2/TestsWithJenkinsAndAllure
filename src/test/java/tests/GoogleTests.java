8033d29f2674e3de6635c4260507b4f7474b3c41
Rinat <rinat.astr2@gmail.com>
2020-08-30 13:49:12 +0600

    added negativetests

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -8,18 +8,26 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+
 
 public class GoogleTests extends TestBase{
     @Test
-    void selenideSearchTest () {
-        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-        Configuration.headless = true;
+    void positiveSelenideSearchTest () {
 
         open("https://www.google.com");
 
         $(byName("q")).setValue("Selenide").pressEnter();
 
         $("html").shouldHave(text("Selenide"));
+    }
+
+    @Test
+    void negativeSelenideSearchTest () {
+
+        open("https://www.google.com");
+
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        $("html").shouldHave(text("ru.selenide.org"));
     }
 }
